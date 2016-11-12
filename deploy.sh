d1624388000775e3fa87f26219dcb2aa6f06ef38
Saurav Modak <saurav@linuxb.in>
2016-11-12 20:17:30 +0530

    rebuilding site Sat Nov 12 20:17:30 IST 2016

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
+# Go To Public folder
+cd public
 # Add changes to git.
 git add -A
 
@@ -17,4 +17,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:inonedamnpage/inonedamnpage.github.io.git gh-pages
+
+# Come Back
+cd ..
